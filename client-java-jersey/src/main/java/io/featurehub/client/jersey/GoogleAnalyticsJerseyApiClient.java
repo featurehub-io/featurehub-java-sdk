9f2f8014b7886c969e2d879173b68b49b27a1dcb
Richard Vowles <richard@bluetrainsoftware.com>
2021-07-10 21:40:51 +1200

    migration of all Java SDKs into their own repository

---
+++ client-java-jersey/src/main/java/io/featurehub/client/jersey/GoogleAnalyticsJerseyApiClient.java
@@ -0,0 +1,22 @@
+package io.featurehub.client.jersey;
+
+import io.featurehub.client.GoogleAnalyticsApiClient;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class GoogleAnalyticsJerseyApiClient implements GoogleAnalyticsApiClient {
+  private final WebTarget target;
+
+  public GoogleAnalyticsJerseyApiClient() {
+    target = ClientBuilder.newBuilder()
+      .build().target("https://www.google-analytics.com/batch");
+  }
+
+  @Override
+  public void postBatchUpdate(String batchData) {
+    target.request().header("Host", "www.google-analytics.com").post(Entity.entity(batchData, MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+  }
+}
