69eba29de02fc651d183d50611b674b27b190efa
Richard Vowles <richard@bluetrainsoftware.com>
2026-04-03 14:57:37 +1300

    cascaded pom updates

--- build_alL_and_test.sh
+++ build_alL_and_test.sh
@@ -2,5 +2,5 @@
 set -x
 MAVEN_OPTS=${MVN_OPTS}
 echo "cd support && mvn -f pom-tiles.xml install && mvn install && cd .. && mvn $MAVEN_OPTS clean install"
-cd support && mvn -f pom-tiles.xml install && mvn install && cd .. && mvn $MAVEN_OPTS clean install
+cd support && mvn -f pom-tiles.xml install && cd .. && mvn $MAVEN_OPTS clean install
 
